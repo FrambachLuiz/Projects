ddb18feeb5b2d120cebdf19b049ac37876961f86
Luiz Augusto Frambach da Cruz <frambachluiz@gmail.com>
2017-07-07 14:11:02 -0300

    Async

--- SOLID/3-encapsulation-solid-m3-single-responsibility-principle-exercise-files/SolidCodeExamples/CodeExamples/SpySink.cs
+++ SOLID/3-encapsulation-solid-m3-single-responsibility-principle-exercise-files/SolidCodeExamples/CodeExamples/SpySink.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Serilog.Core;
@@ -27,5 +28,7 @@ namespace Ploeh.Samples.Encapsulation.CodeExamples
         {
             get { return this.events; }
         }
+
+        
     }
 }
